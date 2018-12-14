42034954f55dcd74ba673f8c52d393b23f0f8ed9
OmarLaham <p.omarlahham@gmail.com>
2018-12-14 11:22:03 +0200

    first commit

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,30 @@
 ## functions do
 
 ## Write a short comment describing this function
-
 makeCacheMatrix <- function(x = matrix()) {
-
+    ic <- NULL
+    set <- function(y) {
+      x <<- y
+      ic <<- NULL
+    }
+    get <- function() x
+    setic <- function(inversecache) ic <<- inversecache
+    getic <- function() ic
+    list(set = set, get = get,
+         setic = setic,
+         getic = getic)
 }
 
 
 ## Write a short comment describing this function
-
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    ic <- x$getic()
+    if(!is.null(ic) && x == x$get()) {
+      message("already cashed")
+      return(ic)
+    }
+    m <- x$get()
+    m <- solve(m, ...)
+    m
 }
